724a076cd4207eb350ddfe32d1f56fe59c971ed7
Sadique110 <117258117+Sadique110@users.noreply.github.com>
2024-02-10 10:33:34 +0000

    ConvertionOfDataType

---
+++ 01_basics/03_ConvertionOperater.js
@@ -0,0 +1,20 @@
+// let score = "33abc"     //NaN means Not A Number
+// let score = "33"    // string
+// let score = "33abc"
+// let score = 33
+// let score = null  // zero
+// let score = true // one for false = 0
+
+// let score = undefined // NaN
+// console.log(typeof (score)); 
+// console.log(typeof score)
+
+// let NumberInValue = Number(score);
+
+// console.log(NumberInValue);
+
+
+let somevalue = 33
+let convertintostring = String(somevalue)
+console.log(typeof(convertintostring))
+console.log(convertintostring)
