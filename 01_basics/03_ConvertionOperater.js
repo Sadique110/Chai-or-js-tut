161042837cfc54c007d40944591708360231793d
Sadique110 <117258117+Sadique110@users.noreply.github.com>
2024-02-15 05:49:44 +0000

    operaters in js

--- 01_basics/03_ConvertionOperater.js
+++ 01_basics/03_ConvertionOperater.js
@@ -18,3 +18,43 @@ let somevalue = 33
 let convertintostring = String(somevalue)
 console.log(typeof(convertintostring))
 console.log(convertintostring)
+
+
+// *************************opreations ********************
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue)
+
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2+2)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+
+// let str1 = "mohammad"
+// let str2 = "sadique"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 1)
+// console.log(1 + "2")
+// console.log("1" + 2 +2);
+// console.log("1" + 3 );
+// console.log( 1 + 2 + "3");
+
+// console.log(+true)
+
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2;
+
+let gemcounter = 100;
+gemcounter++;
+console.log(gemcounter)
