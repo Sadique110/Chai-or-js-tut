df78ed5ff371402194becdfa20b749490c609537
Sadique110 <117258117+Sadique110@users.noreply.github.com>
2024-05-09 06:42:44 +0000

    Number_&_maths

---
+++ 01_basics/08_number-maths.js
@@ -0,0 +1,33 @@
+// const score = 400
+// console.log(score)
+
+// const bilance = new Number(100);
+// console.log(bilance);
+// console.log(bilance.toString().length);
+// console.log(bilance.toFixed(2));
+
+// const otherNumber = 1230.8932
+
+// console.log(otherNumber.toPrecision(3))
+
+// const LocalNumber = 1000000
+// console.log(LocalNumber.toLocaleString())
+// console.log(LocalNumber.toLocaleString('en-IN'))
+
+
+// ===========Maths===============
+
+// console.log(Math)
+// console.log(Math.abs('-4'))
+// console.log(Math.round(23.43))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.2))
+
+
+console.log(Math.random()) // always value in  0 and 1
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * max - min +1 ) + min);
