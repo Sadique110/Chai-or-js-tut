a14ba31466e55d6a1b3bfca122ff11752c485eb3
Sadique110 <117258117+Sadique110@users.noreply.github.com>
2024-05-14 09:02:11 +0000

    functions in js

---
+++ 04_controle_flow/01_contions.js
@@ -0,0 +1,48 @@
+// if
+
+// const isUserLogin = true
+// const tempreture = 43
+// if ( tempreture <= 45 ) {
+//     console.log("good to go");
+// }else{
+//     console.log("denger a ahead")
+// }
+
+// > , < , =>, <=, ==,  !=, !== , ===
+
+// const tempreture = "hello fly"
+// if ( tempreture) {
+//     let power = "fly"
+//     console.log(`User can ${power}`);
+
+// }
+// console.log(`User can ${power}`);
+
+
+// const balance = 1000
+// // if (balance > 500) console.log("yes"), console.log("exciutes"); \\ not a good way to write 
+
+
+// const balance = 1000
+
+// if (bala) {
+    
+// } else {
+    
+// }
+
+
+
+const userloggedIn = true
+const debitCard = true
+const googlelogIn = false
+const EmaillogIn = true
+
+if (userloggedIn && debitCard && 2==3 ) {
+    console.log("User can perches the course");
+}
+
+
+if (googlelogIn || EmaillogIn) {
+    console.log("User logged In");
+}
