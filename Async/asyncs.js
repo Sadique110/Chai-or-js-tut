b9dd5e409c2e7c85cac4b27e1ceb271468732cfd
Mohammad sadique <mohammadsadique110786@gmail.com>
2024-09-13 18:15:23 +0530

    Async in javascript

---
+++ Async/asyncs.js
@@ -0,0 +1,21 @@
+//video link:-->  https://youtu.be/zgt5oTD3rRc?list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37
+// sync and async 
+
+// java script alway sync. sync means code excute line by line 
+
+// js -----
+        // excution context
+            // execute one line of a code at a time 
+
+
+
+
+ /* blocaking code  VS  non blocking code 
+        |                       |
+    blocking the flow of       dosnt not block the execution 
+        program                     
+        |                        |
+    read file sync             read file async
+    
+*/
+
