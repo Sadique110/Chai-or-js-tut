f07fbc6053b56718656ce632cced867a8131ea13
Mohammad sadique <mohammadsadique110786@gmail.com>
2024-10-10 14:00:01 +0530

    switch case in js

---
+++ z_code_with_herry/05_switch_case.js
@@ -0,0 +1,15 @@
+const x = 100
+switch (x) {
+    case 10:
+        console.log("x is 10")
+        break;
+    case 100:
+        console.log("x is 10")
+        break;
+    case 10000:
+        console.log("x is 10000")
+        break;
+    default:
+        console.log("x is not there ")
+        break;
+}
