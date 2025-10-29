d2d16b8adb50a17dc33ef460b9cbe464c3f0d793
mohammad sadique <mohammadsadique110786@gmail.com>
2025-10-29 20:31:03 +0530

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,8 +1,28 @@
+/*
+comparision operater
+1. > greater than
+2. >= greater than equal to
+3. < less than
+4. <= less than equal to
+5. == equal to (value)
+6. === equal to (value and type)
+7. != not equal to (value)
+8. !== not equal to (value and type)
+9. ? ternary operater
+10. && logical and
+11. || logical or   
+*/
 console.log(2 > 1);
 
-// console.log(2 >= 1);
+// console.log(2 >= 1);41
 
-// console.log(2 < 1);
+// 4152063+
+// asdfvbhjnmk,l.;/'
+
+41}"ygtfdresWFGHJM.;'/
+// 052
+// 21onsole.log(;;'/[.POKMGYTVD54C SDDDDDDDDDDDDDF,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,P[;CCCCCCCCC.CCCCCCCCCCCCCCCFT+2 000000FRe k.;/"
+// < 1);
 
 // console.log(2 = 1);
 
